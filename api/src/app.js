709523c1b312b30788fc08b7ec60fe09d8f51266
rz.nakaya@gmail.com <f5w10chotaka@icloud.com>
2025-05-31 20:14:26 +0900

    api作成

--- api/src/app.js
+++ api/src/app.js
@@ -1,55 +1,64 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-import { pool } from './db.js';
+import { pool } from "./db.js";
 
 const app = express();
 
 // REFERENCE: https://developer.mozilla.org/ja/docs/Glossary/CORS
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/posts', async (req, res) => {
+app.get("/posts", async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM post');
+    const result = await pool.query("SELECT * FROM post");
     res.json(result.rows);
   } catch (err) {
-    console.error('DB Error:', err);
-    res.status(500).json({ error: 'Database query failed' });
+    console.error("DB Error:", err);
+    res.status(500).json({ error: "Database query failed" });
   }
 });
 
-app.get('/posts/:id', async (req, res) => {
+app.get("/exhibitions", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM exhibition");
+    res.json(result.rows);
+  } catch (err) {
+    console.error("DB Error:", err);
+    res.status(500).json({ error: "Database query failed" });
+  }
+});
+
+app.get("/posts/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await pool.query('SELECT * FROM post WHERE id = $1', [id]);
+    const result = await pool.query("SELECT * FROM post WHERE id = $1", [id]);
     if (result.rows.length === 0) {
-      return res.status(404).json({ error: 'Post not found' });
+      return res.status(404).json({ error: "Post not found" });
     }
     res.json(result.rows[0]);
   } catch (err) {
-    console.error('DB Error:', err);
-    res.status(500).json({ error: 'Database query failed' });
+    console.error("DB Error:", err);
+    res.status(500).json({ error: "Database query failed" });
   }
 });
 
-
 // WARNING: SQL Injection Attack
 // REFERENCE: https://developer.mozilla.org/ja/docs/Glossary/SQL_Injection
-app.post('/posts', express.json(), async (req, res) => {
+app.post("/posts", express.json(), async (req, res) => {
   const { body } = req.body;
   try {
     const result = await pool.query(
-      'INSERT INTO post (body) VALUES ($1) RETURNING *',
+      "INSERT INTO post (body) VALUES ($1) RETURNING *",
       [body]
     );
     res.status(201).json(result.rows[0]);
   } catch (err) {
-    console.error('DB Error:', err);
-    res.status(500).json({ error: 'Database query failed' });
+    console.error("DB Error:", err);
+    res.status(500).json({ error: "Database query failed" });
   }
 });
 
