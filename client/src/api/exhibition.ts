ab048479c8bfae210e4af2c09434029fd5e649d6
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-06-04 15:31:11 +0900

    chore(package.json): update format script to include CSS files for formatting style(exhibition.ts): standardize import statements and formatting for consistency

--- client/src/api/exhibition.ts
+++ client/src/api/exhibition.ts
@@ -1,10 +1,12 @@
-import { type paths } from '../types/api';
-import { endpoint } from './util';
+import { type paths } from "../types/api";
+import { endpoint } from "./util";
 
-export async function listExihibitions (): Promise<paths['/exhibitions']['get']['responses']['200']['content']['application/json']> {
-  const response = await fetch(endpoint('/exhibitions'));
+export async function listExihibitions(): Promise<
+  paths["/exhibitions"]["get"]["responses"]["200"]["content"]["application/json"]
+> {
+  const response = await fetch(endpoint("/exhibitions"));
   if (!response.ok) {
-    throw new Error('Failed to fetch exhibitions');
+    throw new Error("Failed to fetch exhibitions");
   }
   return response.json();
 }
