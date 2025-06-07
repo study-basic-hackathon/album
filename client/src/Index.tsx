59e8d4463b5cd0ecfed0d17016c77220a3ceb18c
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-06-07 19:23:34 +0900

    fix(Index.tsx): update ExhibitionInfo component to use dynamic id for exhibition data retrieval style(Index.tsx): wrap ExhibitionInfo components in a list for better semantic structure and accessibility

--- client/src/Index.tsx
+++ client/src/Index.tsx
@@ -1,15 +1,14 @@
 import { exhibitions } from "./mocks/data/exhibitions";
 
 function ExhibitionInfo({ id }: { id: number }) {
-  // started_date と ended_date が Date 型であれば、以下の処理は不要になる
-  const startedDate = new Date(exhibitions[1].started_date);
-  const endedDate = new Date(exhibitions[1].ended_date);
+  const startedDate = new Date(exhibitions[id].started_date);
+  const endedDate = new Date(exhibitions[id].ended_date);
 
   return (
     <article>
       <h3>{exhibitions[id].name}</h3>
       <p>
-        {startedDate.toLocaleDateString("ja-JP")} - {endedDate.toLocaleDateString("ja-JP")}
+        開催期間: {startedDate.toLocaleDateString("ja-JP")}-{endedDate.toLocaleDateString("ja-JP")}
       </p>
     </article>
   );
@@ -21,11 +20,23 @@ export default function Index() {
       <h1>華道用写真共有Webアプリ</h1>
       <p>このアプリは、華道の写真を共有するための Web アプリケーションです。</p>
       <h2>華展一覧</h2>
-      <ExhibitionInfo id={1} />
-      <ExhibitionInfo id={2} />
-      <ExhibitionInfo id={3} />
-      <ExhibitionInfo id={4} />
-      <ExhibitionInfo id={5} />
+      <ul role="list">
+        <li>
+          <ExhibitionInfo id={1} />
+        </li>
+        <li>
+          <ExhibitionInfo id={2} />
+        </li>
+        <li>
+          <ExhibitionInfo id={3} />
+        </li>
+        <li>
+          <ExhibitionInfo id={4} />
+        </li>
+        <li>
+          <ExhibitionInfo id={5} />
+        </li>
+      </ul>
     </>
   );
 }
