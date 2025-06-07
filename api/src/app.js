b206e92c4999800905f74c164dbd23f301ec36a1
rz-nakaya <rz.nakaya@gmail.com>
2025-06-07 19:58:34 +0900

    構文エラー/修正

--- api/src/app.js
+++ api/src/app.js
@@ -285,7 +285,7 @@ app.get("/categories/:categoryId", async (req, res) => {
     if (result.rows.length > 0) {
       res.json(result.rows[0]);
     } else {
-      res.status(404).json({ error: "Category not found" });
+      res.status(404).json({ error: "category not found" });
     }
   } catch (err) {
     console.error("DB Error:", err);
@@ -415,7 +415,7 @@ app.get("/seasons/:seasonId", async (req, res) => {
     if (result.rows.length > 0) {
       res.json(result.rows[0]);
     } else {
-      res.status(404).json({ error: "Category not found" });
+      res.status(404).json({ error: "season not found" });
     }
   } catch (err) {
     console.error("DB Error:", err);
@@ -435,7 +435,7 @@ app.get("/seasons/:seasonId/works", async (req, res) => {
           w.title,
           w.arranger_id,
           w.category_id,
-          w.eason_id,
+          w.season_id,
           w.create_date,
           COALESCE(json_agg(DISTINCT wm.material_id) FILTER (WHERE wm.material_id IS NOT NULL), '[]') AS material_ids,
           COALESCE(json_agg(DISTINCT i.url) FILTER (WHERE i.url IS NOT NULL), '[]') AS image_urls
