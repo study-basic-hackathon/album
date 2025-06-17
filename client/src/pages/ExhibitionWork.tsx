c33c10fc9c439b3e4e634014c2b5e677ee6af2b0
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-06-17 15:39:26 +0900

    WARNING: this does NOT work correctly feat(ExhibitionWork.tsx): refactor code to fetch data from API and use hooks

--- client/src/pages/ExhibitionWork.tsx
+++ client/src/pages/ExhibitionWork.tsx
@@ -1,12 +1,12 @@
 import { type components } from "../types/api";
-import { arrangers } from "../mocks/data/arranger";
-import { categories } from "../mocks/data/categories";
-import { exhibitions } from "../mocks/data/exhibitions";
-import { materials } from "../mocks/data/materials";
-import { seasons } from "../mocks/data/seasons";
-import { works } from "../mocks/data/works";
+import { useArranger } from "../api/arranger";
+import { useCategory } from "../api/category";
+import { useExhibition } from "../api/exhibition";
+import { useMaterial } from "../api/material";
+import { useSeason } from "../api/season";
+import { useExhibitionWork } from "../api/exhibition";
 import "./work.css";
-import { useParams, Link } from "react-router";
+import { Link, useParams } from "react-router";
 import WorkImages from "../components/WorkImages";
 import WorkMetadata from "../components/WorkMetadata";
 
@@ -16,12 +16,8 @@ type Exhibition = components["schemas"]["Exhibition"];
 type Material = components["schemas"]["Material"];
 type Season = components["schemas"]["Season"];
 type Work = components["schemas"]["Work"];
+type WorkListItem = components["schemas"]["WorkListItem"];
 type WorkListNavigation = components["schemas"]["WorkListNavigation"];
-
-const workListNavigation: WorkListNavigation = {
-  previous: null,
-  next: 2,
-}; // workListNavigation は仮のデータ
 
 function WorkHeading({ exhibition }: { exhibition: Exhibition }) {
   const title: string = exhibition.name;
@@ -37,7 +33,7 @@ function WorkHeading({ exhibition }: { exhibition: Exhibition }) {
   );
 }
 
-function AdjacentNavigation({ workListNavigation }: { workListNavigation: WorkListNavigation }) {
+function AdjacentNavigation({ navigation }: { navigation: WorkListNavigation }) {
   return (
     <nav className="adjacent-nav">
       <ul>
@@ -53,20 +49,54 @@ function AdjacentNavigation({ workListNavigation }: { workListNavigation: WorkLi
 }
 
 export default function ExhibitionWork() {
-  const { exhibition_id, work_id } = useParams(); // exhibition_id はまだ使わないが、API 関連の実装時におそらく必要になる
-  const work: Work = works[Number(work_id)]; // ToDo: work_id が無効な値のときのエラーハンドリング
-  const arranger: Arranger = arrangers[work.arranger_id];
-  const category: Category = categories[work.category_id];
-  const exhibition: Exhibition = exhibitions[work.exhibition_id]; // ToDo: exhibition_id が null の場合の処理
-  const materialArray: Material[] = work.material_ids.map((material_id) => materials[material_id]);
-  const season: Season = seasons[work.season_id];
+  const params = useParams();
+  const exhibitionId = Number(params.exhibitionId);
+  const workId = Number(params.workId);
+  const workListItem: WorkListItem = useExhibitionWork(exhibitionId, workId);
+  console.log("workListItem", workListItem);
+  const work: Work = workListItem?.work;
+  const navigation: WorkListNavigation = workListItem?.navigation;
 
+  if (!workListItem) {
+    return (
+      <main>
+        <h1>ローディング中</h1>
+      </main>
+    );
+  } else {
+    return (
+      <>
+        <main>
+          <WorkImages work={work} />
+        </main>
+      </>
+    );
+  }
+  /*
+  const arranger: Arranger = useArranger(work?.arranger_id);
+  const category: Category = useCategory(work?.category_id);
+  const exhibition: Exhibition = useExhibition(work?.exhibition_id);
+  const materialArray: Material[] = work?.material_ids.map((material_id) =>
+    useMaterial(material_id)
+  );
+  const season: Season = useSeason(work?.season_id);
+  */
+  /*
+  if (!workListItem) {
+    return (
+      <main>
+        <h1>指定された作品は存在しません</h1>
+      </main>
+    );
+  }
+  */
+  /*
   return (
     <>
       <main>
         <WorkHeading exhibition={exhibition} />
         <WorkImages work={work} />
-        <AdjacentNavigation workListNavigation={workListNavigation} />
+        <AdjacentNavigation navigation={navigation} />
         <WorkMetadata
           arranger={arranger}
           category={category}
@@ -78,4 +108,5 @@ export default function ExhibitionWork() {
       </main>
     </>
   );
+  */
 }
