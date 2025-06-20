eb9655197fa0790378e3cfe8d6aebeaed4ea8e4b
YOKOYAMA Taku <seekseep@gmail.com>
2025-06-20 19:01:24 +0900

    画像ファイルのアップロード

--- api/src/repositories/image.js
+++ api/src/repositories/image.js
@@ -1,8 +1,14 @@
 import path from 'path';
 import fs from 'fs';
 
+const UPLAODS_DIRECTORY = process.env.UPLAODS_DIRECTORY || "uploads";
+
+function getUploadsDirectoryPath() {
+  return path.resolve(UPLAODS_DIRECTORY);
+}
+
 export function findImageById(imageId) {
-  const uploadDir = path.resolve("upload");
+  const uploadDir = getUploadsDirectoryPath();
   const files = fs.readdirSync(uploadDir);
   const foundFileName = files.find((file) => path.parse(file).name === imageId);
   if (foundFileName === undefined) {
