1dbd6cd1b3ebab9c2fc549633e48bad688c94195
YOKOYAMA Taku <seekseep@gmail.com>
2025-06-20 09:28:59 +0900

    for deploy

--- api/src/server.js
+++ api/src/server.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || 'localhost';
 
 app.listen(PORT, HOST, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://${HOST}:${PORT}`);
 });
