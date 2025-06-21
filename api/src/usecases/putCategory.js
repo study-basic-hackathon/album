444a26582ebb1c16a28cfa155c2bd34a3ab4d2fd
rz-nakaya <rz.nakaya@gmail.com>
2025-06-21 23:09:09 +0900

    カテゴリの更新の追加/修正

--- api/src/usecases/putCategory.js
+++
@@ -1,7 +0,0 @@
-import { putCategory } from '../repositories/putCategory.js';
-
-// カテゴリの更新
-export async function putCategory(categoryId, name) {
-  const result = await putCategory(categoryId, name);
-  return result[0];
-};
