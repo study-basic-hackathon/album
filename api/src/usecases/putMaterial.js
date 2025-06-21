54cfe10c8ad95ad5ce8671c1fb27831458256a28
rz-nakaya <rz.nakaya@gmail.com>
2025-06-21 23:16:31 +0900

    花材の更新の追加/修正

--- api/src/usecases/putMaterial.js
+++
@@ -1,7 +0,0 @@
-import { putMaterial } from '../repositories/putMaterial.js';
-
-// 花材の更新
-export async function putMaterial(materialId, name) {
-  const result = await putMaterial(materialId, name);
-  return result[0];
-};
