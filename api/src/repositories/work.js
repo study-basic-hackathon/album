a3e58911d4eaefa4e0be2cc60edccb18cd83809a
rz-nakaya <rz.nakaya@gmail.com>
2025-06-22 00:34:29 +0900

    作品の更新の追加/作成途中

--- api/src/repositories/work.js
+++ api/src/repositories/work.js
@@ -1,22 +1,43 @@
 import { pool } from "../db.js";
 
-export async function updateWork(workId, title, arrangerId, materialId, season, categoryId, imageIds) {
+export async function updateWork(workId, title, arranger_id, season_id, category_id ) {
     const result = await pool.query(`
         UPDATE 
             work
         SET
             title = $2,
-            arrenger_id = $3,
-            material_id = $4,
-            season = $5,
-            category_id = $6,
-            image_ides = $7
+            arranger_id = $3,
+            season_id = $4,
+            category_id = $5
         WHERE
             id = $1
         RETURNING 
             *
         `,  
-        [workId, title, arrangerId, materialId, season, categoryId, imageIds]
+        [workId, title, arranger_id, season_id, category_id ]
     );
     return result.rows;
 };
+
+// export async function findWorksByMaterialId(materialId) {
+//     const result = await pool.query(`
+//         SELECT
+//             wk.id,
+//             wk.title,
+//             wk.arranger_id,
+//             wk.material_id,
+//             wk.season_id,
+//             wk.category_id,
+//             TO_CHAR(wk.created_at, 'YYYY-MM-DD') AS created_at
+//         FROM
+//             work AS wk
+//         JOIN
+//             work_material AS wm ON wk.id = wm.work_id
+//         WHERE
+//             wm.material_id = $1
+//         ORDER BY
+//             wk.created_at ASC`, 
+//         [materialId]
+//     );
+//     return result.rows;
+// }{
