21d52c17239b4f7b19a4e9de9e39929f770b744b
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-07-01 17:02:10 +0900

    feat(WorksImages): refactor image list to use CSS modules for better styling management feat(Arranger, Category, Exhibition, Material, Season): replace h1 with Heading component for consistent heading styling chore(works-images.module.scss): add styles for WorksImages component to improve layout and responsiveness

--- client/src/pages/Exhibition.tsx
+++ client/src/pages/Exhibition.tsx
@@ -3,6 +3,7 @@ import "./works.css"; // ToDo: CSS のインポートの変更
 import { useParams } from "react-router";
 import { useExhibition, useExhibitionWorkListItems } from "../hooks/exhibition";
 import WorksImages from "../components/WorksImages";
+import Heading from "../components/Heading";
 
 type Exhibition = components["schemas"]["Exhibition"];
 type Work = components["schemas"]["Work"];
@@ -21,7 +22,7 @@ export default function Exhibition() {
 
   return (
     <>
-      <h1>{`${exhibition.name}の作品一覧`}</h1>
+      <Heading title={`${exhibition.name}の作品一覧`} />
       <WorksImages works={exhibitionWorks} />
     </>
   );
