1af6fe4c17712c7df86197a0e5582387aadc8484
Hayochan-man <taropado3.14@gmail.com>
2025-07-05 16:28:00 +0900

    imageにおけるpost処理の追加

--- api/src/routers/image.js
+++ api/src/routers/image.js
@@ -1,7 +1,41 @@
 import express from "express";
-import { getImageById } from '../usecases/image.js';
+import multer from "multer";
+import fs from "fs";
+import path from "path";
+import { getImageById, createImage } from '../usecases/image.js';
 
 const router = express.Router();
+
+const uploadDir = path.resolve("./uploads");
+fs.mkdirSync(uploadDir, { recursive: true });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    const imageId = createImage();
+    const ext = path.extname(file.originalname) || ".png";
+    const filename = `${imageId}${ext}`;
+    cb(null, filename);
+  },
+});
+
+const uploadImage = multer({ storage });
+
+// 画像の登録
+router.post("/", uploadImage.single("file"), (req, res) => {
+  try{
+    if (!req.file) {
+      return res.status(400).json({ error: "Invalid file" });
+    }
+    const imageId = path.parse(req.file.filename).name;
+    res.status(201).header("Location", `/images/${imageId}`);
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+    };
+});
 
 // 画像の取得
 router.get("/:imageId", async (req, res) => {
