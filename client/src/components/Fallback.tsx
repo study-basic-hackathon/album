10a9bc0430fcb3d3489b3b53b29fcf99bac856c8
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-07-06 14:08:23 +0900

    fix(component): change the fallback component to use a div instead of a section to improve semantic correctness

--- client/src/components/Fallback.tsx
+++ client/src/components/Fallback.tsx
@@ -2,8 +2,8 @@ import styles from "./scss/fallback.module.scss";
 
 export default function Fallback({ message, isError }: { message: string; isError?: boolean }) {
   return (
-    <section className={styles.fallback} role="status">
+    <div className={styles.fallback} role="status">
       <p className={isError ? styles.error : styles.normal}>{message}</p>
-    </section>
+    </div>
   );
 }
