22eb1f7e1dd4c334200221a8a0dee39cced128bd
peaks-and-valleys <39687819+peaks-and-valleys@users.noreply.github.com>
2025-07-06 18:39:37 +0900

    feat(client.tsx): add Head component to set document title and meta tags

---
+++ client/src/components/Head.tsx
@@ -0,0 +1,10 @@
+export default function Head({ title, description }: { title: string; description?: string }) {
+  return (
+    <>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+    </>
+  );
+}
